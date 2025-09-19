2ce4e5c956e05adb84d1dab4b21f13aaee2a9295
default <marko.skorupan@stu.ocsb.ca>
2025-09-19 13:01:40 +0000

    Made program print I love COmputer Science

---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ * @author Marko Skorupan
+ * @version 1.0.0
+ * @date 2025-09-19
+ * @fileoverview This program practices with string laterals.
+ */
+
+//string literal text
+console.log("I love computer Science!!");
+
+// string literal text
+console.log("    I  love Computer Science");
+
+//string literal text
+console.log("I   love   Computer   Science   100%");
+
+console.log("\nDone");
